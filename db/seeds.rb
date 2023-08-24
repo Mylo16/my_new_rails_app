0621929803f54cfc5a7f53d6fbce04d8290ec55f
Eric Antwi <ericopokuantwi384@gmail.com>
2023-08-24 20:27:03 +0000

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Message.create([{ greeting: 'Good Morning' }, { greeting: 'Good day' }, { greeting: 'Good evening' }, { greeting: 'Good afternoon' }, { greeting: 'Good night' }])
